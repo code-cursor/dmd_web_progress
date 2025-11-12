80744e475ea296668e2a8a408199608a67b8040a
forhadhub <mfhe.2223.bd@gmail.com>
2025-11-12 12:46:16 +0600

    update privacy policy page

--- js/privacy_policy.js
+++ js/privacy_policy.js
@@ -1,9 +1,21 @@
-$(document).ready(function () {
-  $(".accordion-button").first().addClass("selected");
-  $(".accordion-button").click(function () {
-    $(".accordion-button").removeClass("selected");
-    $(this).addClass("selected");
+  // Fade-up animation when scrolling
+  $(window).on("scroll", function() {
+    $(".gallery-item scroll-reveal").each(function() {
+      const position = $(this).offset().top;
+      const scroll = $(window).scrollTop();
+      const windowHeight = $(window).height();
+      if (scroll + windowHeight > position + 100) {
+        $(this).addClass("show");
+      }
+    });
   });
 
-  $(".patient-container").css("opacity", 0).animate({ opacity: 1 }, 1000);
-});
+  // Scroll reveal animation
+    window.addEventListener("scroll", () => {
+    document.querySelectorAll(".scroll-reveal").forEach((el) => {
+        const rect = el.getBoundingClientRect().top;
+        if (rect < window.innerHeight - 100) {
+        el.classList.add("visible");
+        }
+    });
+    });
